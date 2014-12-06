11597e396a8ff57e66a6dcd66fdc20a64d879faf
Antoine FRESSE <afresse54@gmail.com>
2014-12-06 17:06:26 +0100

    trucs de vendredi matin

---
+++ unity/Assets/Scripts/Zombie.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour {
+
+
+    public int MaxLife = 5;
+
+    private int _life;
+
+	void Start () {
+	    _life = MaxLife;
+	}
+
+
+    public void OnHit() {
+        _life -= 1;
+        if (_life == 0) {
+            Destroy(gameObject);
+        }
+    }
+}
