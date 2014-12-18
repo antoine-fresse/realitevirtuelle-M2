cea57d694157a820d601b03676e31cd3b8619f32
Benoit <dumesnil.benoit@gmail.com>
2014-12-18 15:41:37 +0100

    Ajout d'une classe permettant de faire apparaitre des zombies. Modification pour parcourir plusieurs splines.

---
+++ unity/Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public int size;
+    public int number;
+    public GameObject prefab;
+    public float rate;
+
+    private float timeSinceLastSpawn;
+    private int numberLeft;
+    private bool started;
+
+	// Use this for initialization
+	void Start () {
+        numberLeft = number;
+        timeSinceLastSpawn = (1.0f / rate);
+        started = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (started) {
+            timeSinceLastSpawn += Time.deltaTime;
+            if (timeSinceLastSpawn >= (1.0f/rate)) {
+                numberLeft--;
+                timeSinceLastSpawn = 0f;
+
+                float x = (Random.value * 2f * size) - size;
+                float z = (Random.value * 2f * size) - size;
+                Vector3 pos = transform.localPosition + new Vector3(x, 0, z);
+                Instantiate(prefab, pos, Quaternion.identity);
+            }
+        }
+	}
+
+    public void Spawn() {
+        started = true;
+    }
+}
--- unity/Assets/Scripts/Spline/SplineWalker.cs
+++ unity/Assets/Scripts/Spline/SplineWalker.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SplineWalker : MonoBehaviour {
 
-	public BezierSpline spline;
+	public GameObject splineGameObject;
+
+    private List<BezierSpline> splines;
+
+    private BezierSpline currentSpline;
+    private int currentSplineIndex = 0;
 
 	public float duration;
 
@@ -13,34 +19,57 @@ public class SplineWalker : MonoBehaviour {
 	private float progress;
 	private bool goingForward = true;
 
-	private void Update () {
-		if (goingForward) {
-			progress += Time.deltaTime / duration;
-			if (progress > 1f) {
-				if (mode == SplineWalkerMode.Once) {
-					progress = 1f;
-				}
-				else if (mode == SplineWalkerMode.Loop) {
-					progress -= 1f;
-				}
-				else {
-					progress = 2f - progress;
-					goingForward = false;
-				}
-			}
-		}
-		else {
-			progress -= Time.deltaTime / duration;
-			if (progress < 0f) {
-				progress = -progress;
-				goingForward = true;
-			}
-		}
+    private void Start() {
+        splines = new List<BezierSpline>();
+        foreach (Transform child in splineGameObject.transform) {
+            BezierSpline s = child.gameObject.GetComponent<BezierSpline>();
+            if (s != null) {
+                splines.Add(s);
+            }
+        }
+    }
 
-		Vector3 position = spline.GetPoint(progress);
-		transform.localPosition = position;
-		if (lookForward) {
-			transform.LookAt(position + spline.GetDirection(progress));
-		}
+	private void Update () {
+        if (currentSpline != null) {
+            if (goingForward) {
+                progress += Time.deltaTime / duration;
+                if (progress > 1f) {
+                    if (mode == SplineWalkerMode.Once) {
+                        progress = 1f;
+                    } else if (mode == SplineWalkerMode.Loop) {
+                        progress -= 1f;
+                    } else {
+                        progress = 2f - progress;
+                        goingForward = false;
+                    }
+                }
+            } else {
+                progress -= Time.deltaTime / duration;
+                if (progress < 0f) {
+                    progress = -progress;
+                    goingForward = true;
+                }
+            }
+            print(progress);
+            Vector3 position = currentSpline.GetPoint(progress);
+            transform.localPosition = position;
+            if (lookForward) {
+                transform.LookAt(position + currentSpline.GetDirection(progress));
+            }
+            if (progress >= 1f) {
+                currentSpline = null;
+                progress = 0f;
+                currentSplineIndex++;
+            }
+        }
+        /*if (Input.GetKeyDown("space")) {
+            nextSpline();
+        }*/
 	}
+
+    private void nextSpline() {
+        if ((currentSplineIndex < splines.Count) && (currentSpline == null)) {
+            currentSpline = splines[currentSplineIndex];
+        }
+    }
 }
